79190fc5c863d58fc8966bb7e6dd9379e14b27d1
Valentin Filyov <valentin.filyov@athlonsofia.com>
2023-09-13 17:12:24 +0300

    SND-526: Local Storage Provider and Integration with Mock provider

--- src/routes/driver-poc/RPCProviderConfig.ts
+++ src/routes/driver-poc/RPCProviderConfig.ts
@@ -1,23 +1,25 @@
 import APIProvider from './APIProvider';
 import BaseProvider from './BaseProvider';
 import MockProvider from './MockProvider';
+import StorageProvider from './StorageProvider';
 
 export default class RPCProviderConfig {
   providers = {
     MOCK: new MockProvider(),
     API: new APIProvider(),
+    STORAGE: new StorageProvider()
   };
   map: Record<string, BaseProvider> = {
-    getCandidateList: this.providers.API,
-    getPackages: this.providers.API,
-    getCandidateInfo: this.providers.API,
+    getCandidateList: this.providers.STORAGE,
+    getPackages: this.providers.STORAGE,
+    getCandidateInfo: this.providers.STORAGE,
     enrollCandidate: this.providers.API,
     getBeneficiariesList: this.providers.API,
     validateBeneficiaries: this.providers.API,
     executePayments: this.providers.API,
     login: this.providers.API,
     getRoles: this.providers.API,
-    createCandidate: this.providers.API,
+    createCandidate: this.providers.STORAGE,
     updateCandidate: this.providers.API,
     deleteCandidate: this.providers.API,
   };
